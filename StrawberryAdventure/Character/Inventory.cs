18367495a5eea2cbea5867a2bfb1946ba20965c4
AngelAngelov77 <ah40@abv.bg>
2017-01-08 05:36:12 +0200

    Update Inventory.cs

--- StrawberryAdventure/Character/Inventory.cs
+++ StrawberryAdventure/Character/Inventory.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 
 namespace StrawberryAdventure
 {
     class Inventory : IInventory
     {
         BasicItem[] _items;
+        int _backpack = (int) ItemPositions.Backpack1 - 1;
 
         public Inventory()
         {
@@ -15,33 +16,68 @@ namespace StrawberryAdventure
         {
             get
             {
-                throw new NotImplementedException();
+                return _items;
             }
         }
 
         public void EquipItem(BasicItem item)
         {
-            throw new NotImplementedException();
+            if (_items[(int)item.BodyPosition] != null)
+            {
+                Unequip((ItemPositions)item.BodyPosition);
+            };
+            _items[(int)item.BodyPosition] = item;
         }
 
         public void EquipItem(BasicItem item, bool removeAlreadyEquippedItem)
         {
-            throw new NotImplementedException();
+            if (_items[(int)item.BodyPosition] == null || removeAlreadyEquippedItem)
+            {
+                EquipItem(item);
+            }
+            else
+            {
+                if (_backpack < (int)ItemPositions.BackpackLast)
+                {
+                    _backpack++;
+                    _items[_backpack] = item;
+                }
+            }
         }
 
         public BasicItem GetItem(ItemPositions position)
         {
-            throw new NotImplementedException();
+            return _items[(int)position];
         }
-
-        public void RemoveItem(BasicItem item, BodyPositions position = BodyPositions.Backpack)
+        public void RemoveItem(ItemPositions position)
         {
-            throw new NotImplementedException();
+            if (position >= ItemPositions.Backpack1)
+            {
+                for (int i = (int)position + 1; i <= (int) ItemPositions.BackpackLast; i++)
+                {
+                    _items[i - 1] = _items[i];
+                    _backpack--;
+                }
+            }
+            else
+            {
+                Unequip(position);
+            }
         }
 
         public void Unequip(ItemPositions position)
         {
-            throw new NotImplementedException();
+            if (position >= ItemPositions.Backpack1)
+            {
+                RemoveItem(position);
+            }
+            if (_backpack < (int) ItemPositions.BackpackLast)
+            {
+                _backpack++;
+                _items[_backpack] = _items[(int)position];
+            }
+            _items[(int)position] = null;
         }
+
     }
 }
