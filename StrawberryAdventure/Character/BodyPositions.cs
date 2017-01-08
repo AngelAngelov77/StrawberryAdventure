c1144255eff661e2102741ea5bece5797b068d84
AngelAngelov77 <ah40@abv.bg>
2017-01-08 05:33:17 +0200

    Update BodyPositions.cs

--- StrawberryAdventure/Character/BodyPositions.cs
+++ StrawberryAdventure/Character/BodyPositions.cs
@@ -2,46 +2,40 @@
 {
     public enum BodyPositions
     {
-        Head = 1,
-        Neck = 2,
-        Body = 4,
-        Leggings = 8,
-        Feet = 16,
-        LeftHand = 32,
-        RightHand = 64,
-        Shoulders = 128,
-        LeftRing = 256,
-        RightRing = 512,
-        Belt = 1024,
-        Hands = 2048,
-        Backpack = 4096,
-        Rings = LeftRing | RightRing
+        LeftHand = 0,
+        RightHand,
+        Head,
+        Neck,
+        Body,
+        Leggings,
+        Feet,
+        Hands,
+        Fingers,
+        Shoulders,
+        Backpack
     }
 
     public enum ItemPositions
     {
-        Head = 0,
-        Neck = 1,
-        Body = 2,
-        Leggings = 3,
-        Feet = 4,
-        LeftHand = 5,
-        RightHand = 6,
-        Shoulders = 7,
-        LeftRing = 8,
-        RightRing = 9,
-        Hands = 10,
-        Belt1 = 11,
-        Belt2 = 12,
-        Belt3 = 13,
-        Belt4 = 14,
-        Backpack1 = 15,
+        LeftHand = 0,
+        RightHand,
+        Head,
+        Neck,
+        Body,
+        Leggings,
+        Feet,
+        Hands,
+        Fingers,
+        Shoulders,
+        Backpack1,
         Backpack2,
         Backpack3,
         Backpack4,
         Backpack5,
         Backpack6,
         Backpack7,
+        BackpackLast,
         Count
     }
+
 }
