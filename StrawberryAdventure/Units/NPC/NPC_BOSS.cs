9eeb9cc200571b27d1c26a278865386ff3c46a4c
ekov1 <kristian_ekov@abv.bg>
2017-01-15 00:13:58 +0200

    class boss

---
+++ StrawberryAdventure/Units/NPC/NPC_BOSS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrawberryAdventure.Units.NPC
+{
+    class NPC_BOSS : INPC
+    {
+        BasicItem INPC.DroppingItems
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        BasicItem INPC.ItemsDropped()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            //TODO
+            //attack plyer
+        }
+    }
+}
